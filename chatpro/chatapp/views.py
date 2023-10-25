dd9fc28c9553fd71815c20910ec05ca44a06991c
Coder202220 <123199573+Coder202220@users.noreply.github.com>
2023-10-25 14:47:20 +0530

    Update views.py

--- chatpro/chatapp/views.py
+++ chatpro/chatapp/views.py
@@ -3,11 +3,11 @@ from .chatbot_logic import chatbot
 
 def chatbot_view(request):
     if request.method == 'POST':
-        user_message = request.POST['user_message']
-        response = chatbot.get_response(user_message)
+        user = request.POST['user_message']
+        bot = chatbot.get_response(user_message)
     else:
         response = "Hello, how can I assist you?"
 
-    return render(request, 'chat.html', {'response': response})
+    return render(request, 'chat.html', {'response': bot})
 
 # Create your views here.
